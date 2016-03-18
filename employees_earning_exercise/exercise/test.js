7e992cf90389744ea64265577e18592bff7a4158
Roman Dementev <roman.dementev@hexlet-source.com>
2016-03-18 13:57:35 +0300

    fix test

--- employees_earning_exercise/exercise/test.js
+++ employees_earning_exercise/exercise/test.js
@@ -1,28 +1,30 @@
 /* global require describe it */
 
-var fs = require("fs");
-var assert = require("assert");
-var pgp = require("pg-promise")(/*options*/);
-var cn = {
-  user: 'nobody',
-  host: '/var/run/postgresql'
+const fs = require("fs");
+const assert = require("assert");
+const pgp = require("pg-promise")(/*options*/);
+const cn = {
+  user: "nobody",
+  host: "/var/run/postgresql"
 };
 
 describe("rows", function() {
-  "use strict";
   it("should be updated", (done) => {
-    var db = pgp(cn);
-    const solution = fs.readFileSync("./solution.sql", "utf8").split(/--.*/).map(query => query.trim()).filter(query => query !== "");
+    const db = pgp(cn);
+    const solution = fs.readFileSync("./solution.sql", "utf8")
+                       .split(/--.*/)
+                       .map(query => query.trim())
+                       .filter(query => query !== "");
 
-var userSolutionChain = solution.reduce((acc, line) => {
-  return acc.then(() => db.query(line));
-}, Promise.resolve());
+    const userSolutionChain = solution.reduce((acc, line) => {
+      return acc.then(() => db.query(line));
+    }, Promise.resolve());
 
-var solutionPromise = Promise.resolve().then(() => db.query("select solution() AS name;"));
+    const solutionPromise = Promise.resolve().then(() => db.query("select solution() AS name;"));
 
-Promise.all([userSolutionChain, solutionPromise]).then((values) => {
-  assert.equal(JSON.stringify(values[0]), JSON.stringify(values[1]));
-}).then(done, done);
+    Promise.all([userSolutionChain, solutionPromise]).then((values) => {
+      assert.equal(JSON.stringify(values[0]), JSON.stringify(values[1]));
+    }).then(done, done);
 
   });
 });
